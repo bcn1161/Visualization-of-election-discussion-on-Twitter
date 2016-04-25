22925ab101ed918c815066f4a8a934c2f01fad39
bcn1161 <yz2859@columbia.edu>
2016-04-25 10:51:41 -0400

    tweet collection

--- data collection.R
+++ data collection.R
@@ -7,6 +7,7 @@ consumerSecret <- "(your consumer secret here)"
 my_oauth <- OAuthFactory$new(consumerKey = consumerKey, consumerSecret = consumerSecret, requestURL = requestURL, accessURL = accessURL, authURL = authURL)
 my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
 save(my_oauth, file = "my_oauth.Rdata")
+
 library(streamR)
 load("~/my_oauth.Rdata")
 filterStream("tweet.json", track = c("bernie","sanders", "hillary", "clinton", "o'malley"), timeout = 6000, oauth = my_oauth)
